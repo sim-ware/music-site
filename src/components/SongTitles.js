d4a59ced8d40530e051b90003e1a7c1133b405df
Samir Gossain <samir.gossain@gmail.com>
2019-12-17 20:48:27 +0000

    trying to push

--- src/components/SongTitles.js
+++ src/components/SongTitles.js
@@ -5,11 +5,10 @@ export default function SongTitles() {
     <div style={{ margin: '65px 4px 4px 4px' }}>
       <code>1:Acid Saturday</code><br/> <br/>
       <code>2:YouAre</code> <br/> <br/>
-      <code>3:Haunts</code><br/> <br/>
-      <code>4:Hurts</code><br/> <br/>
-      <code>5:SatFlangeGuitar</code><br/> <br/>
-      <code>6:SatSoothingGuitar</code><br/> <br/>
-      <code>7:ScarTissue</code><br/> <br/>
+      <code>3:FarOut</code><br/> <br/>
+      <code>4:Haunts</code><br/> <br/>
+      <code>5:SatSoothingGuitar</code><br/> <br/>
+      <code>6:ScarTissue</code><br/> <br/>
     </div>
   );
 }
